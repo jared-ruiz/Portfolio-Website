f387c202e312b4eb3bdf5b16c9d024eb3b556272
Jared Ruiz <jared707@gmail.com>
2022-07-27 18:05:15 -0500

    Added cards from bulma styling and rough draft of current portfolio tab. Incorporated state and passing state as a prop to other components.

---
+++ src/components/Photos/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+// import Modal from "../Modal";
+
+const Photos = (props) => {
+    const projects = props.projectsList;
+
+    return(
+        <div className="columns">
+            {projects.map((project) => (
+                <div className="column mt-6">
+                    <div className="card p-3">
+
+                        <div className="card-image">
+                            <figure class="image is-5by3">
+                                <img src={project.image} alt="Placeholder image"/>
+                            </figure>
+                        </div>
+
+                        <div className="m-3">
+                            <div>
+                                <p className="title is-4">{project.title}</p>
+                                <a className="button is-fullwidth is-dark is-outlined my-2" href={project.github} target='_blank'>GitHub</a>
+                                <a className="button is-fullwidth is-dark is-outlined" href={project.deployLink} target='_blank'>Deployed</a>
+                            </div>
+                        </div>
+                        
+                        <div className="mx-2">
+                            {project.description}
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Photos;
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Photos from '../Photos';
+
+//project images
+import img1 from '../../assets/project-images/project-1.PNG'
+import img2 from '../../assets/project-images/project-2.PNG'
+import img3 from '../../assets/project-images/project-3.PNG'
 
 const Portfolio = () => {
+
+    //project information
+    const [projectList, setProjectList] = useState([
+        {
+            // id: 1,
+            title: 'Patio',
+            image: img1,
+            description: "Local bar locator using geographic location API's and brewery API's to locate top 5 destinations near you.",
+            github: 'https://github.com/kyrotomato/Patio',
+            deployLink: 'https://kyrotomato.github.io/Patio/'
+        },
+        {
+            // id: 2,
+            title: 'Fantasy Chat',
+            image: img2,
+            description: "Online role playing application with ability to store posts and comments for every campaign's needs.",
+            github: 'https://github.com/jared-ruiz/Fantasy-Chat',
+            deployLink: 'https://vast-refuge-25222.herokuapp.com/login'
+        },
+        {
+            // id: 3,
+            title: 'Current',
+            image: img3,
+            description: "An all in one link sharing application to document and show off the most important links in your life.",
+            github: 'https://github.com/JonCurls/current',
+            deployLink: 'https://desolate-crag-78193.herokuapp.com/'
+        }
+    ])
+
     return(
-        <h1>Test</h1>
+        <Photos projectsList={projectList} />
     )
 }
 
