6e70dbb4935c00930d5fa73652448617dbe416cc
Jared Ruiz <jared707@gmail.com>
2022-07-29 04:14:30 -0500

    Added state change to nav links

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,6 +7,10 @@ const Nav = () => {
     //navbar state
     const [navBarActive, setNavBarActive] = useState(false);
 
+    const closeNavBar = () => {
+        setNavBarActive(false);
+    }
+
     return(
         <nav className='is-justify-content-center background is-size-4-tablet font-color'>
             <div id='mobile-button'>
@@ -14,23 +18,21 @@ const Nav = () => {
             </div>
             <ul id={navBarActive ? 'hidden' : ''}>
                 <li>
-                    <Link to='/'>Home</Link>
+                    <Link to='/' onClick={closeNavBar}>Home</Link>
                 </li>
                 <li>
-                    <Link to='/about'>About</Link>
+                    <Link to='/about' onClick={closeNavBar}>About</Link>
                 </li>
                 <li>
-                    <Link to='/portfolio'>Portfolio</Link>
+                    <Link to='/portfolio' onClick={closeNavBar}>Portfolio</Link>
                 </li>
                 <li>
-                    <Link to='/contact'>Contact</Link>
+                    <Link to='/contact' onClick={closeNavBar}>Contact</Link>
                 </li>
                 <li>
-                    <Link to='/resume'>Resume</Link>
+                    <Link to='/resume' onClick={closeNavBar}>Resume</Link>
                 </li>
             </ul>
-            
-
         </nav>
     )
 }
